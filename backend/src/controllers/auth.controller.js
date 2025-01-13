cf3d3b8c593aff648ecfb55c0be79ce64b7f858d
salsabeel10 <salsabeelibrahim327@gmail.com>
2025-01-13 20:48:31 +0400

    login and logout done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -51,10 +51,41 @@ export const signup =async (req, res) => {
   }
 }
 
-export const login = (req, res) => {
-  res.send('login up')
+export const login =async (req, res) => {
+  const {email,password} = req.body
+  try {
+    //check user exist
+    const user = await User.findOne({email})
+    if(!user){
+      return res.status(400).json({message:"invalid credentials"})
+    }
+    //check password
+    const isPasswordCorrect = await bcrypt.compare(password,user.password)
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'invalid credentials' })
+    }
+
+    generateToken(user._id,res)
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    })
+
+  } catch (error) {
+    console.log('error in signup controller', error.message)
+    res.status(500).json({ message: 'internal server error' })
+  }
 }
 
-export const logout = (req, res) => {
-  res.send('logout up')
+export const logout =(req, res) => {
+  try {
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({ message: 'Logout SuccessFully' })
+  } catch (error) {
+    console.log('error in signup controller', error.message)
+    res.status(500).json({ message: 'internal server error' })
+  }
 }
