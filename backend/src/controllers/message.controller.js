a01a552a2467052220c6d2c550fb8435c4c60c17
salsabeel10 <salsabeelibrahim327@gmail.com>
2025-01-14 13:43:33 +0400

    get messages route done

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,14 +1,38 @@
-import User from "../models/user.model.js";
+import Message from '../models/message.model.js'
+import User from '../models/user.model.js'
 
-export const getUsersForSidebar =async (req,res )=>{
-    try {
-        //get userid from middleware
-        const loggedInUser=req.user._id;
-        const filteredUser =await User.find({_id:{$ne:loggedInUser}}).select("-password");
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    //get userid from middleware
+    const loggedInUser = req.user._id
+    const filteredUser = await User.find({ _id: { $ne: loggedInUser } }).select(
+      '-password'
+    )
 
-        res.status(200).json(filteredUser)
-    } catch (error) {
-        console.log('error in getUserForSideBar', error.message)
-        res.status(500).json({ message: 'internal server error' })
-    }
+    res.status(200).json(filteredUser)
+  } catch (error) {
+    console.log('error in getUserForSideBar', error.message)
+    res.status(500).json({ message: 'internal server error' })
+  }
+}
+
+export const getMessages = async (req, res) => {
+  try {
+    //get from params
+    const { id: userToChatId } = req.params
+    //get from middleware
+    const myId = req.user._id
+
+    //select all messages from logged user and receiver
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    })
+    res.status(200).json({ messages })
+  } catch (error) {
+    console.log('error in getMessages', error.message)
+    res.status(500).json({ message: 'internal server error' })
+  }
 }
