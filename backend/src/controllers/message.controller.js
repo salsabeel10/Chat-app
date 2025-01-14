2ba4f98818f45ba8dabb1a7bd26361e94e101aff
salsabeel10 <salsabeelibrahim327@gmail.com>
2025-01-14 14:01:11 +0400

    post message done

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from '../lib/cloudinary.js'
 import Message from '../models/message.model.js'
 import User from '../models/user.model.js'
 
@@ -35,4 +36,33 @@ export const getMessages = async (req, res) => {
     console.log('error in getMessages', error.message)
     res.status(500).json({ message: 'internal server error' })
   }
+}
+
+export const sendMessage =async (req,res )=>{
+    try {
+        const {text,image} = req.body;
+        const{id:receiverId} =req.params;
+        const senderId = req.user._id;
+        //convert image to url
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl =uploadResponse.secure_url;
+        }
+
+        const newMessage =new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl,
+        });
+        //save to message db
+        await newMessage.save()
+
+        //todo here code for socketIo
+        res.status(201).json(newMessage)
+    } catch (error) {
+        console.log('error in sendMessage', error.message)
+        res.status(500).json({ message: 'internal server error' })
+    }
 }
