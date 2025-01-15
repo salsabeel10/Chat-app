2ec64df6b5a9180378a243fb6595d3df094b3649
salsabeel10 <salsabeelibrahim327@gmail.com>
2025-01-15 20:55:49 +0400

    initial setup for ui done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from './routes/message.route.js'
 import dotenv from 'dotenv';
 import { connectDB } from "./lib/db.js";
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -13,6 +14,12 @@ const port = process.env.PORT||5005
 //middleware for extract data from body
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials:true
+  })
+)
 
 app.use('/api/auth',authRoutes)
 app.use('/api/message', messageRoutes)
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,24 @@
+import { create } from 'zustand'
+import { axiosInstance } from '../lib/axios'
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIng: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      //url get from axios file
+      const res = axiosInstance.get('auth/check')
+      set({ authUser: res.data })
+    } catch (error) {
+      console.log('error in check auth', error)
+      set({ authUser: null })
+    } finally {
+      set({ isCheckingAuth: false })
+    }
+  },
+}))
