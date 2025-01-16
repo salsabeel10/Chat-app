84ca1943ca9d94b9568597b64a75454174f23cf6
salsabeel10 <salsabeelibrahim327@gmail.com>
2025-01-16 13:55:18 +0400

    minor adjustment

--- frontend/src/components/AuthImagePatter.jsx
+++ frontend/src/components/AuthImagePatter.jsx
@@ -2,7 +2,7 @@ const AuthImagePattern = ({ title, subtitle }) => {
   return (
     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center pt-7">
-        <div className="grid grid-cols-3 gap-3 mb-8">
+        <div className="grid grid-cols-3 gap-3 mb-8 mt-2">
           {[...Array(9)].map((_, i) => (
             <div
               key={i}
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
     <div className="h-screen grid lg:grid-cols-2">
       {/* Left Side - Form */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
-        <div className="w-full max-w-md space-y-8">
+        <div className="w-full max-w-md space-y-8 mb-14">
           {/* Logo */}
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
