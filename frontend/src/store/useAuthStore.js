0e46436b5ba99c0cbb7fd50c3e8cad5c3d4b6fbc
salsabeel10 <salsabeelibrahim327@gmail.com>
2025-01-16 13:44:13 +0400

    completed login and signup page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import { axiosInstance } from '../lib/axios'
+import toast from 'react-hot-toast'
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -21,7 +22,38 @@ export const useAuthStore = create((set) => ({
       set({ isCheckingAuth: false })
     }
   },
-  signUp:async () =>{
-    
-  }
+
+  signUp: async (data) => {
+    set({ isSigningUp: true })
+    try {
+      const res = await axiosInstance.post('auth/signup', data)
+      set({ authUser: res.data })
+      toast.success('Account Created Successfully')
+    } catch (error) {
+      toast.error(error.response.data.message)
+    } finally {
+      set({ isSigningUp: false })
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post('auth/logout')
+      set({ authUser: null })
+      toast.success('User Logged Out SuccessFully')
+    } catch (error) {
+      toast.error(error.response.data.message)
+    }
+  },
+  login: async (data) => {
+    set({ isLoggingIng: true })
+    try {
+      const res = await axiosInstance.post('auth/login', data)
+      set({ authUser: res.data })
+      toast.success('Logged In Successfully')
+    } catch (error) {
+      toast.error(error.response.data.message)
+    } finally {
+      set({ isLoggingIng: false })
+    }
+  },
 }))
